6712982302de52175db2a19e93d236bb838f7073
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-12-09 10:47:44 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,29 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
+import PropTypes from 'prop-types';
 
 export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
+  static defaultProps = {
+    initialContacts: [],
+    initialFilter: '',
   };
 
-  componentDidUpdate(prevState) {
-    const prevStateContacts = prevState.contacts;
-    const nextStayContacts = this.state.contacts;
+  static propTypes = {
+    initialContacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
+    initialFilter: PropTypes.string.isRequired,
+  };
 
-    if (prevStateContacts !== nextStayContacts) {
-      localStorage.setItem('ContactListUl', JSON.stringify(nextStayContacts));
-    }
-  }
+  state = {
+    contacts: this.props.initialContacts,
+    filter: this.props.initialFilter,
+  };
 
   handleChange = e => {
     const { name, value } = e.target;
